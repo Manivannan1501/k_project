b8765b5df33a52b5533118122e279f80f9b22ece
Manivannan1501 <manivannank1503@gmail.com>
2025-07-24 11:35:10 +0530

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import joblib
 import pickle
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans, DBSCAN
